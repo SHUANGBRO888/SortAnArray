7730080653df4d82a58835d38366ea974d8de89e
SHUANGBRO888 <deng.shua@northeastern.edu>
2023-02-28 20:02:38 -0800

    1st Sort An Array in C++

---
+++ SortAnArray.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+void sort(int array[], int size);
+
+int main()
+{
+
+    int array[] = {10,6,21,1,9,8,12,11,19,2,3};
+    int size = sizeof(array)/sizeof(array[0]);
+
+    sort(array,size);
+
+    for(int element : array){
+        std::cout<< element <<" ";
+    }
+
+
+    return 0;
+}
+
+
+void sort(int array[], int size){
+    
+    int temp; 
+    for(int i =0; i<size; i++){
+        for(int j =0; j<size-i-1; j++){
+            if (array[j]>=array[j+1])
+            {
+                temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }           
+        }
+    }
+}
